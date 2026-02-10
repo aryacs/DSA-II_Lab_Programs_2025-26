bf3a42b6e43f0f2b7772e9543b97e1f068e7ba40
aryacs <aryanchandrapal@gmail.com>
2026-02-10 09:56:18 +0530

    some files added and some files modified. Please check the commit message for details.

--- Unit-I/Unit-II/BTInorder.c
+++ Unit-I/Unit-II/BTInorder.c
@@ -1,26 +1,23 @@
 #include<stdio.h>
-#include<stdlib.h>  
-struct node
-{
+#include<stdlib.h>
+struct node{
     int data;
     struct node *left;
     struct node *right;
 };
-struct node* create(int val)
-{
-    struct node* root = (struct node*)malloc(sizeof(struct node));
+stuct node *create(int val){
+    struct node *root = (struct node*)malloc(sizeof(struct node));
     root->data = val;
     root->left = NULL;
     root->right = NULL;
     return root;
 }
-void inorder(struct node* root)
-{
-    if (root == NULL)
+void Inorder(struct node *root){
+    if(root == NULL)
         return;
-    inorder(root->left);
+    Inorder(root->left);
     printf("%d ", root->data);
-    inorder(root->right);
+    Inorder(root->right);
 }
 int main(){
 struct node* root = create(5);
@@ -32,7 +29,7 @@ root->right->left = create(40);
 root->right->right = create(50);
 root->left->right->left = create(30);
 root->left->right->right = create(35);
-printf("The Inorder Traversal is=>\n");
-inorder(root);
+printf("\nThe Inorder Traversal of Binary Tree is=>\n");
+Inorder(root);
 return 0;
 }
